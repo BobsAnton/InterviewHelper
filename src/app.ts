928674d94aaf6abba6461a0f67802717fd87e4e0
Anton Aleksandrov <v-antonale@microsoft.com>
2022-08-30 17:24:38 +0200

    connect to MongoDB, install mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
 import express from "express";
+import mongoose from "mongoose";
 import path from "path";
 const app = express();
 const port = 8080;
 
-app.get( "/", ( req, res ) => {
-    res.sendFile('index.html', {root: path.join(__dirname, "..") });
+app.get( "/", async (req, res) => {
+    mongoose.connect('mongodb://localhost:27017/', async function(err) {
+        if (err) throw err;
+
+        const customerSchema = new mongoose.Schema({
+            name: String,
+            address: String
+        });
+    
+        const Customer = mongoose.model('Customer', customerSchema);
+
+        const customer1 = new Customer({ name: 'John', address: 'Highway 71'});
+        await customer1.save();
+        console.log(await Customer.find());
+
+        res.sendFile('index.html', {root: path.join(__dirname, "..") });
+    });
 } );
 
 app.listen( port, () => {
