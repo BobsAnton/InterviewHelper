8f6e6fb830c58c0d5dae968a31195d1d3a705ea7
Anton Aleksandrov <v-antonale@microsoft.com>
2022-09-14 12:09:08 +0200

    fix

--- src/routes/questions.ts
+++ src/routes/questions.ts
@@ -4,48 +4,16 @@ import { TechnicalFieldModel } from '../models/technical-field';
 const router = express.Router();
 
 router.get('/', async function(req, res, next) {
-	res.send(await QuestionModel.find({}).populate('technicalField'));
+	try {
+		res.send(await QuestionModel.find({}).populate('technicalField'));
+	}
+	catch (err) {
+		console.error(err);
+	}
 });
 
 router.post('/', async function(req, res, next) {
-	let technicalFieldFromDb = await TechnicalFieldModel.findOne({ name: req.body.technicalFieldName });
-	if (technicalFieldFromDb === null)
-	{
-		res.status(404);
-		res.send("TechnicalField not found!");
-		return;
-	}
-
-	let questionFromDb = await QuestionModel.findOne({ name: req.body.questionName });
-	if (questionFromDb !== null) 
-	{
-		res.status(409);
-		res.send("Question already exists!");
-		return;
-	}
-
-	let newQuestion = new QuestionModel({
-		name: req.body.questionName,
-		description: req.body.questionDescription,
-		complexity: req.body.questionComplexity,
-		technicalField: technicalFieldFromDb._id
-	});
-	await newQuestion.save();
-	res.status(201);
-	res.send(newQuestion);
-});
-
-router.patch('/', async function(req, res, next) {
-	let questionFromDb = await QuestionModel.findOne({ name: req.body.questionName });
-	if (questionFromDb === null)
-	{
-		res.status(404);
-		res.send("Question not found!");
-		return;
-	}
-
-	if (req.body.technicalFieldName !== null)
-	{
+	try {
 		let technicalFieldFromDb = await TechnicalFieldModel.findOne({ name: req.body.technicalFieldName });
 		if (technicalFieldFromDb === null)
 		{
@@ -53,89 +21,151 @@ router.patch('/', async function(req, res, next) {
 			res.send("TechnicalField not found!");
 			return;
 		}
-		await QuestionModel.updateOne(
-			{ name: req.body.questionName },
-			{ 
-				description: req.body.questionDescription,
-				complexity: req.body.questionComplexity,
-				technicalField: technicalFieldFromDb._id
-			});
-	}
-	else 
-	{
-		await QuestionModel.updateOne(
-			{ name: req.body.questionName },
-			{ 
-				description: req.body.questionDescription,
-				complexity: req.body.questionComplexity
-			});
-	}
+	
+		let questionFromDb = await QuestionModel.findOne({ name: req.body.name });
+		if (questionFromDb !== null)
+		{
+			res.status(409);
+			res.send("Question already exists!");
+			return;
+		}
+	
+		let newQuestion = new QuestionModel({
+			name: req.body.name,
+			description: req.body.description,
+			complexity: req.body.complexity,
+			technicalField: technicalFieldFromDb._id
+		});
+		await newQuestion.save();
 
-	questionFromDb = await QuestionModel.findOne({ name: req.body.questionName });
+		questionFromDb = await QuestionModel.findOne({ name: req.body.name }).populate('technicalField');
 
-	res.status(200);
-	res.send(questionFromDb);
+		res.status(201);
+		res.send(questionFromDb);
+	}
+	catch (err) {
+		console.error(err);
+	}
+});
+
+router.patch('/', async function(req, res, next) {
+	try {
+		let questionFromDb = await QuestionModel.findOne({ name: req.body.name });
+		if (questionFromDb === null)
+		{
+			res.status(404);
+			res.send("Question not found!");
+			return;
+		}
+	
+		if (req.body.technicalFieldName !== null)
+		{
+			let technicalFieldFromDb = await TechnicalFieldModel.findOne({ name: req.body.technicalFieldName });
+			if (technicalFieldFromDb === null)
+			{
+				res.status(404);
+				res.send("TechnicalField not found!");
+				return;
+			}
+			await QuestionModel.updateOne(
+				{ name: req.body.name },
+				{ 
+					description: req.body.description,
+					complexity: req.body.complexity,
+					technicalField: technicalFieldFromDb._id
+				});
+		}
+		else 
+		{
+			await QuestionModel.updateOne(
+				{ name: req.body.name },
+				{ 
+					description: req.body.description,
+					complexity: req.body.complexity
+				});
+		}
+	
+		questionFromDb = await QuestionModel.findOne({ name: req.body.name });
+	
+		res.status(200);
+		res.send(questionFromDb);
+	}
+	catch (err) {
+		console.error(err);
+	}
 });
 
 router.delete('/', async function(req, res, next) {
-	let questionFromDb = await QuestionModel.findOne({ name: req.body.questionName });
-	if (questionFromDb === null)
-	{
-		res.status(404);
-		res.send("Question not found!");
-		return;
+	try {
+		let questionFromDb = await QuestionModel.findOne({ name: req.body.name });
+		if (questionFromDb === null)
+		{
+			res.status(404);
+			res.send("Question not found!");
+			return;
+		}
+
+		const result = await QuestionModel.findOne({ name: req.body.name }).populate('technicalField');
+	
+		await QuestionModel.deleteOne({ name: req.body.name });
+	
+		questionFromDb = await QuestionModel.findOne({ name: req.body.name });
+	
+		if (questionFromDb !== null){
+			res.status(304);
+			res.send("Error! Question wasn't deleted!");
+			return;
+		}
+	
+		res.status(200);
+		res.send(result);
 	}
-
-	await QuestionModel.deleteOne({ name: req.body.questionName });
-
-	questionFromDb = await QuestionModel.findOne({ name: req.body.questionName });
-
-	if (questionFromDb !== null){
-		res.status(304);
-		res.send("Error! Question wasn't deleted!");
-		return;
+	catch (err) {
+		console.error(err);
 	}
-
-	res.status(200);
-	res.send("Question was deleted successfully!");
 });
 
 router.put('/', async function(req, res, next) {
-	let technicalFieldFromDb = await TechnicalFieldModel.findOne({ name: req.body.technicalFieldName });
-	if (technicalFieldFromDb === null)
-	{
-		res.status(404);
-		res.send("TechnicalField not found!");
-		return;
+	try {
+		let technicalFieldFromDb = await TechnicalFieldModel.findOne({ name: req.body.technicalFieldName });
+		if (technicalFieldFromDb === null)
+		{
+			res.status(404);
+			res.send("TechnicalField not found!");
+			return;
+		}
+	
+		let questionFromDb = await QuestionModel.findOne({ name: req.body.name });
+		if (questionFromDb !== null)
+		{
+			await QuestionModel.updateOne(
+				{ name: req.body.name },
+				{ 
+					description: req.body.description,
+					complexity: req.body.complexity,
+					technicalField: technicalFieldFromDb._id
+				});
+	
+				questionFromDb = await QuestionModel.findOne({ name: req.body.name });
+	
+			res.status(200);
+			res.send(questionFromDb);
+			return;
+		}
+	
+		let newQuestion = new QuestionModel({
+			name: req.body.name,
+			description: req.body.description,
+			complexity: req.body.complexity,
+			technicalField: technicalFieldFromDb._id
+		});
+		await newQuestion.save();
+		res.status(201);
+		res.send(newQuestion);
 	}
-
-	let questionFromDb = await QuestionModel.findOne({ name: req.body.questionName });
-	if (questionFromDb !== null)
-	{
-		await QuestionModel.updateOne(
-			{ name: req.body.questionName },
-			{ 
-				description: req.body.questionDescription,
-				complexity: req.body.questionComplexity,
-				technicalField: technicalFieldFromDb._id
-			});
-
-			questionFromDb = await QuestionModel.findOne({ name: req.body.questionName });
-
-		res.status(200);
-		res.send(questionFromDb);
-		return;
+	catch (err) {
+		console.error(err);
 	}
-
-	let newQuestion = new QuestionModel({
-		name: req.body.questionName,
-		description: req.body.questionDescription,
-		complexity: req.body.questionComplexity,
-		technicalField: technicalFieldFromDb._id
-	});
-	await newQuestion.save();
-	res.status(201);
-	res.send(newQuestion);
 });
 
 export const questionsRouter = router;
--- src/routes/technical-fields.ts
+++ src/routes/technical-fields.ts
@@ -1,98 +1,132 @@
 import express from 'express';
 import { TechnicalFieldModel } from '../models/technical-field';
+import { QuestionModel } from '../models/question';
 
 const router = express.Router();
 
 router.get('/', async function(req, res, next) {
-	res.send(await TechnicalFieldModel.find({}));
+	try {
+		res.send(await TechnicalFieldModel.find({}));
+	}
+	catch (err) {
+		console.error(err);
+	}
 });
 
 router.post('/', async function(req, res, next) {
-	let technicalFieldFromDb = await TechnicalFieldModel.findOne({ name: req.body.technicalFieldName });
-	if (technicalFieldFromDb !== null)
-	{
-		res.status(409);
-		res.send("TechnicalField already exists!");
-		return;
+	try {
+		let technicalFieldFromDb = await TechnicalFieldModel.findOne({ name: req.body.name });
+		if (technicalFieldFromDb !== null)
+		{
+			res.status(409);
+			res.send("TechnicalField already exists!");
+			return;
+		}
+	
+		let newTechnicalField = new TechnicalFieldModel({
+			name: req.body.name,
+			order: req.body.order
+		});
+		await newTechnicalField.save();
+		res.status(201);
+		res.send(newTechnicalField);
 	}
-
-	let newTechnicalField = new TechnicalFieldModel({
-		name: req.body.technicalFieldName,
-		order: req.body.technicalFieldOrder
-	});
-	await newTechnicalField.save();
-	res.status(201);
-	res.send(newTechnicalField);
+	catch (err) {
+		console.error(err);
+	}
 });
 
 router.patch('/', async function(req, res, next) {
-	let technicalFieldFromDb = await TechnicalFieldModel.findOne({ name: req.body.technicalFieldName });
-	if (technicalFieldFromDb === null)
-	{
-		res.status(404);
-		res.send("TechnicalField not found!");
-		return;
+	try {
+		let technicalFieldFromDb = await TechnicalFieldModel.findOne({ name: req.body.name });
+		if (technicalFieldFromDb === null)
+		{
+			res.status(404);
+			res.send("TechnicalField not found!");
+			return;
+		}
+	
+		await TechnicalFieldModel.updateOne(
+			{ name: req.body.name },
+			{ 
+				order: req.body.order
+			});
+	
+		technicalFieldFromDb = await TechnicalFieldModel.findOne({ name: req.body.name });
+	
+		res.status(200);
+		res.send(technicalFieldFromDb);
 	}
-
-	await TechnicalFieldModel.updateOne(
-		{ name: req.body.technicalFieldName },
-		{ 
-			order: req.body.technicalFieldOrder
-		});
-
-	technicalFieldFromDb = await TechnicalFieldModel.findOne({ name: req.body.technicalFieldName });
-
-	res.status(200);
-	res.send(technicalFieldFromDb);
+	catch (err) {
+		console.error(err);
+	}
 });
 
 router.delete('/', async function(req, res, next) {
-	let technicalFieldFromDb = await TechnicalFieldModel.findOne({ name: req.body.technicalFieldName });
-	if (technicalFieldFromDb === null)
-	{
-		res.status(404);
-		res.send("TechnicalField not found!");
-		return;
+	try {
+		let technicalFieldFromDb = await TechnicalFieldModel.findOne({ name: req.body.name });
+		if (technicalFieldFromDb === null)
+		{
+			res.status(404);
+			res.send("TechnicalField not found!");
+			return;
+		}
+
+		const result = await TechnicalFieldModel.findOne({ name: req.body.name });
+	
+		let questionFromDb = await QuestionModel.find({ technicalField: technicalFieldFromDb._id });
+		if (questionFromDb !== null)
+		{
+			await QuestionModel.deleteMany({ technicalField: technicalFieldFromDb._id });
+		}
+
+		await TechnicalFieldModel.deleteOne({ name: req.body.name });
+	
+		technicalFieldFromDb = await TechnicalFieldModel.findOne({ name: req.body.name });
+	
+		if (technicalFieldFromDb !== null){
+			res.status(304);
+			res.send("Error! TechnicalField wasn't deleted!");
+			return;
+		}
+	
+		res.status(200);
+		res.send(result);
 	}
-
-	await TechnicalFieldModel.deleteOne({ name: req.body.technicalFieldName });
-
-	technicalFieldFromDb = await TechnicalFieldModel.findOne({ name: req.body.technicalFieldName });
-
-	if (technicalFieldFromDb !== null){
-		res.status(304);
-		res.send("Error! TechnicalField wasn't deleted!");
-		return;
+	catch (err) {
+		console.error(err);
 	}
-
-	res.status(200);
-	res.send("TechnicalField was deleted successfully!");
 });
 
 router.put('/', async function(req, res, next) {
-	let technicalFieldFromDb = await TechnicalFieldModel.findOne({ name: req.body.technicalFieldName });
-	if (technicalFieldFromDb !== null)
-	{
-		await TechnicalFieldModel.updateOne(
-			{ name: req.body.technicalFieldName },
-			{ 
-				order: req.body.technicalFieldOrder
-			});
-
-		technicalFieldFromDb = await TechnicalFieldModel.findOne({ name: req.body.technicalFieldName });
-
-		res.status(200);
-		res.send(technicalFieldFromDb);
-		return;
+	try {
+		let technicalFieldFromDb = await TechnicalFieldModel.findOne({ name: req.body.name });
+		if (technicalFieldFromDb !== null)
+		{
+			await TechnicalFieldModel.updateOne(
+				{ name: req.body.name },
+				{ 
+					order: req.body.order
+				});
+	
+			technicalFieldFromDb = await TechnicalFieldModel.findOne({ name: req.body.name });
+	
+			res.status(200);
+			res.send(technicalFieldFromDb);
+			return;
+		}
+	
+		let newTechnicalField = new TechnicalFieldModel({
+			name: req.body.name,
+			order: req.body.order
+		});
+		await newTechnicalField.save();
+		res.status(201);
+		res.send(newTechnicalField);
 	}
-
-	let newTechnicalField = new TechnicalFieldModel({
-		name: req.body.technicalFieldName,
-		order: req.body.technicalFieldOrder
-	});
-	await newTechnicalField.save();
-	res.status(201);
-	res.send(newTechnicalField);
+	catch (err) {
+		console.error(err);
+	}
 });
 
 export const technicalFieldsRouter = router;
